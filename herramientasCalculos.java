ae910d9cbbde13e113a52d22c9ecb07d244f7a87
yahir110 <brandomyahirsantos@gmail.com>
2026-03-11 14:14:30 +0000

    avance cotizador

--- herramientasCalculos.java
+++ herramientasCalculos.java
@@ -1,1 +1,19 @@
+public static void costedeViaje(int adultos, int niños) {
+int tarifaV = 0;
+int suma = 0;
+int descuento = mod%50;
+costedeViaje(adultos=tarifaV, niños=tarifaV*descuento);
+suma=costedeViaje;
 
+
+}
+
+public static void costoHospedaje(int adultos, int niños) {
+int tarifaH = mod%30;
+int tarifaC = 0;
+costoHospedaje(adultos=tarifaC , niños=tarifaC*tarifaH);
+
+}
+
+
+public static 
